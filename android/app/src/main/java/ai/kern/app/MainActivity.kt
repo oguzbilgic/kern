dc16682b0673b06be799cc688ab6e2f238e1e8af
dshaubedlio <david.shaub@edlio.com>
2026-04-03 22:24:46 -0400

    build 40: fix race - KernNative check instead of _kernNativeBuild

--- android/app/src/main/java/ai/kern/app/MainActivity.kt
+++ android/app/src/main/java/ai/kern/app/MainActivity.kt
@@ -24,7 +24,7 @@ import com.google.android.material.textfield.TextInputEditText
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        const val BUILD = 39
+        const val BUILD = 40
     }
 
     private lateinit var webView: WebView
